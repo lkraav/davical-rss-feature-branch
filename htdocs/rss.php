d3ec7747e6d7d4d20c1ddc749aad3b14f5a05df0
leho <leho@kraav.com>
2010-12-19 23:56:28 +0200

    rss.php: great success extracting a SUMMARY of a VEVENT and sticking it into feed item <title>

--- htdocs/rss.php
+++ htdocs/rss.php
@@ -71,124 +71,33 @@ function caldav_get_rss( $request ) {
 	  }
 
 	  /**
-	  * Here we are constructing a whole calendar response for this collection, including
+	  * Here we are constructing the RSS feed response for this collection, including
 	  * the timezones that are referred to by the events we have selected.
+	  * Library used: http://framework.zend.com/manual/en/zend.feed.writer.html
 	  */
-	  $vcal = new iCalComponent();
-	  $vcal->VCalendar();
-	  $displayname = $dav_resource->GetProperty('displayname');
-	  if ( isset($displayname) ) {
-		$vcal->AddProperty("X-WR-CALNAME", $displayname);
-	  }
+    require_once('Zend/Feed/Writer/Feed.php');
+    $feed = new Zend_Feed_Writer_Feed;
 
-	  $need_zones = array();
-	  $timezones = array();
-	  while( $event = $qry->Fetch() ) {
-		$ical = new iCalComponent( $event->caldav_data );
+    $feed->setTitle('CalDAV RSS Feed: '. $dav_resource->GetProperty('displayname'));
+    $feed->setLink('http://www.google.com');
+    $feed->setDescription('This is test of creating a Zend Feed.');
 
-		/** Save the timezone component(s) into a minimal set for inclusion later */
-		$event_zones = $ical->GetComponents('VTIMEZONE',true);
-		foreach( $event_zones AS $k => $tz ) {
-		  $tzid = $tz->GetPValue('TZID');
-		  if ( !isset($tzid) ) continue ;
-		  if ( $tzid != '' && !isset($timezones[$tzid]) ) {
-			$timezones[$tzid] = $tz;
-		  }
-		}
+    $need_zones = array();
+    $timezones = array();
+    while( $event = $qry->Fetch() ) {
+      $ical = new iCalComponent( $event->caldav_data );
+      $event_data = $ical->GetComponents('VEVENT',true);
+      $item = $feed->createEntry();
+      $item->setTitle($event_data[0]->GetPValue('SUMMARY'));
+      $feed->addEntry($item);
+      break;
+    }
 
-		/** Work out which ones are actually used here */
-		$comps = $ical->GetComponents('VTIMEZONE',false);
-		foreach( $comps AS $k => $comp ) {
-		  $tzid = $comp->GetPParamValue('DTSTART', 'TZID');      if ( isset($tzid) && !isset($need_zones[$tzid]) ) $need_zones[$tzid] = 1;
-		  $tzid = $comp->GetPParamValue('DUE',     'TZID');      if ( isset($tzid) && !isset($need_zones[$tzid]) ) $need_zones[$tzid] = 1;
-		  $tzid = $comp->GetPParamValue('DTEND',   'TZID');      if ( isset($tzid) && !isset($need_zones[$tzid]) ) $need_zones[$tzid] = 1;
-
-		  if ( $dav_resource->HavePrivilegeTo('all',false) || $session->user_no == $event->user_no || $session->user_no == $event->logged_user
-				|| ( $c->allow_get_email_visibility && $comp->IsAttendee($session->email) ) ) {
-			/**
-			* These people get to see all of the event, and they should always
-			* get any alarms as well.
-			*/
-			$vcal->AddComponent($comp);
-			continue;
-		  }
-		  /** No visibility even of the existence of these events if they aren't admin/owner/attendee */
-		  if ( $event->class == 'PRIVATE' ) continue;
-
-		  if ( ! $dav_resource->HavePrivilegeTo('DAV::read') || $event->class == 'CONFIDENTIAL' ) {
-		   $vcal->AddComponent(obfuscated_event($comp));
-		  }
-		  elseif ( isset($c->hide_alarm) && $c->hide_alarm ) {
-			// Otherwise we hide the alarms (if configured to)
-			$comp->ClearComponents('VALARM');
-			$vcal->AddComponent($comp);
-		  }
-		  else {
-			$vcal->AddComponent($comp);
-		  }
-		}
-	  }
-
-	  /** Put the timezones on there that we need */
-	  foreach( $need_zones AS $tzid => $v ) {
-		if ( isset($timezones[$tzid]) ) $vcal->AddComponent($timezones[$tzid]);
-	  }
-
-	  $response = $vcal->Render();
+    $response = $feed->export('rss');
 	  header( 'Content-Length: '.strlen($response) );
 	  header( 'Etag: '.$dav_resource->unique_tag() );
-	  $request->DoResponse( 200, ($request->method == 'HEAD' ? '' : $response), 'text/calendar; charset="utf-8"' );
+	  $request->DoResponse( 200, ($request->method == 'HEAD' ? '' : $response), 'text/xml; charset="utf-8"' );
 	}
-
-
-	// Just a single event then
-
-	$resource = $dav_resource->resource();
-	$ic = new iCalComponent( $resource->caldav_data );
-
-	/** Default deny... */
-	$allowed = false;
-	if ( $dav_resource->HavePrivilegeTo('all', false) || $session->user_no == $resource->user_no || $session->user_no == $resource->logged_user
-		  || ( $c->allow_get_email_visibility && $ic->IsAttendee($session->email) ) ) {
-	  /**
-	  * These people get to see all of the event, and they should always
-	  * get any alarms as well.
-	  */
-	  $allowed = true;
-	}
-	else if ( $resource->class != 'PRIVATE' ) {
-	  $allowed = true; // but we may well obfuscate it below
-	  if ( ! $dav_resource->HavePrivilegeTo('DAV::read') || ( $resource->class == 'CONFIDENTIAL' && ! $request->HavePrivilegeTo('DAV::write-content') ) ) {
-		$ical = new iCalComponent( $resource->caldav_data );
-		$comps = $ical->GetComponents('VTIMEZONE',false);
-		$confidential = obfuscated_event($comps[0]);
-		$ical->SetComponents( array($confidential), $resource->caldav_type );
-		$resource->caldav_data = $ical->Render();
-	  }
-	}
-	// else $resource->class == 'PRIVATE' and this person may not see it.
-
-	if ( ! $allowed ) {
-	  $request->DoResponse( 403, translate("Forbidden") );
-	}
-
-	header( 'Etag: "'.$resource->dav_etag.'"' );
-	header( 'Content-Length: '.strlen($resource->caldav_data) );
-
-	$contenttype = 'text/plain';
-	switch( $resource->caldav_type ) {
-	  case 'VJOURNAL':
-	  case 'VEVENT':
-	  case 'VTODO':
-		$contenttype = 'text/calendar';
-		break;
-
-	  case 'VCARD':
-		$contenttype = 'text/vcard';
-		break;
-	}
-
-	$request->DoResponse( 200, ($request->method == 'HEAD' ? '' : $resource->caldav_data), $contenttype.'; charset="utf-8"' );
 }
 
 if ( $request->method == 'GET' ) {
@@ -197,7 +106,7 @@ if ( $request->method == 'GET' ) {
 else {
     dbg_error_log( 'rss', 'Unhandled request method >>%s<<', $request->method );
     dbg_log_array( 'rss', '_SERVER', $_SERVER, true );
-	dbg_error_log( 'rss', 'RAW: %s', str_replace("\n", '',str_replace("\r", '', $request->raw_post)) );
+    dbg_error_log( 'rss', 'RAW: %s', str_replace("\n", '',str_replace("\r", '', $request->raw_post)) );
 }
 
 $request->DoResponse( 500, translate('The application program does not understand that request.') );
